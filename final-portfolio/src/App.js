7e9d37eda9912ccb30e9899f5c8f85cd261c09dc
Justin Durocher <justduro@gmail.com>
2020-05-07 18:12:34 -0400

    ready

--- final-portfolio/src/App.js
+++ final-portfolio/src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import NavBar from './components/navBar.js';
+
+import Contact from './components/contact.js';
+
+import Portfolio from './components/portfolio.js';
+
+import About from './components/about.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <BrowserRouter>
+
+      <NavBar></NavBar>
+
+      <Switch>
+
+        <Route path="/" component={About} exact />
+        <Route path="/portfolio" component={Portfolio} exact/>
+        <Route path="/contact" component={Contact} exact/>
+
+      </Switch>
+      
+      </BrowserRouter>
+
     </div>
+      
   );
 }
 
