411514715b8e0e3fb94abbd2c45c3eb8f1075926
Justin Durocher <justduro@gmail.com>
2020-05-07 19:22:35 -0400

    ready

--- final-portfolio/src/components/navBar.js
+++ final-portfolio/src/components/navBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import * as Bootstrap from 'react-bootstrap';
+import {Nav, Navbar} from 'react-bootstrap';
 
 import './css/navBar.css';
 
@@ -10,15 +10,15 @@ class navbar extends Component {
     render(){
         return(
 
-            <Bootstrap.Navbar bg="info" variant="dark" sticky="top">
-            <Bootstrap.Navbar.Brand href="#home">Justin Durocher</Bootstrap.Navbar.Brand>
-            <Bootstrap.Nav className="mr-auto">
-              <Bootstrap.Nav.Link href="/">About Me</Bootstrap.Nav.Link>
-              <Bootstrap.Nav.Link href="/portfolio">Projects</Bootstrap.Nav.Link>
-              <Bootstrap.Nav.Link href="https://docs.google.com/document/d/16A5Z64eX0jHCl3c2y4zLkMO4XZwj6elAXm6BUK64ct4/edit?usp=sharing">Resume</Bootstrap.Nav.Link>
-              <Bootstrap.Nav.Link href="/contact">Contact Me</Bootstrap.Nav.Link>
-            </Bootstrap.Nav>
-            </Bootstrap.Navbar>
+            <Navbar bg="info" variant="dark" sticky="top">
+            <Navbar.Brand href="#home">Justin Durocher</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="/">About Me</Nav.Link>
+              <Nav.Link href="/portfolio">Projects</Nav.Link>
+              <Nav.Link href="https://docs.google.com/document/d/16A5Z64eX0jHCl3c2y4zLkMO4XZwj6elAXm6BUK64ct4/edit?usp=sharing" target="_blank">Resume</Nav.Link>
+              <Nav.Link href="/contact">Contact Me</Nav.Link>
+            </Nav>
+            </Navbar>
 
         )
     }
